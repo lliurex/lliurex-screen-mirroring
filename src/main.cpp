f0b504bfa0a383c9c2af8a809ef2292a53f8ef6f
Lt-Henry <quiqueiii@gmail.com>
2021-11-29 13:47:24 +0100

    Basic project and empty window

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+/*
+    Lliurex screen mirroring
+
+    Copyright (C) 2021  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QIcon>
+#include <QQuickView>
+#include <QQmlContext>
+#include <QQmlEngine>
+#include <QScreen>
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    QApplication app(argc,argv);
+    QApplication::setApplicationName("LliureX Screen Mirroring");
+    QApplication::setApplicationVersion("1.0");
+    
+    QQuickView *view = new QQuickView;
+    view->setIcon(QIcon::fromTheme("monitor"));
+    view->setMinimumSize(QSize(400,360));
+    view->setMaximumSize(QSize(400,360));
+    
+    QQmlContext* ctxt = view->rootContext();
+    QObject::connect(ctxt->engine(),&QQmlEngine::exit,&app,&QCoreApplication::exit);
+    
+    view->setSource(QUrl(QStringLiteral("qrc:/ui.qml")));
+    
+    view->show();
+    
+    // center on screen
+    QScreen* screen = view->screen();
+    uint32_t x = screen->geometry().width()/2 - view->frameGeometry().width()/2;
+    uint32_t y = screen->geometry().height()/2 - view->frameGeometry().height()/2;
+    view->setPosition(x,y);
+    
+    return app.exec();
+}
---
+++ src/ui.qml
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 Lliurex project
+ *
+ * Author:
+ *  Enrique Medina Gremaldos <quiqueiii@gmail.com>
+ *
+ * Source:
+ *  https://github.com/lliurex/lliurex-screen-mirroring
+ *
+ * This file is a part of lliurex screen mirroring
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.kirigami 2.16 as Kirigami
+
+import QtQuick 2.6
+import QtQuick.Controls 2.6 as QQC2
+import QtQuick.Layouts 1.15
+import QtQuick.Dialogs 1.0
+
+QQC2.Pane {
+    id: main
+    
+    visible:true
+    width: 400
+    height: 360
+    anchors.centerIn: parent
+    
+}
